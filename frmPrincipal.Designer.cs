dd785b5c2910fadc1d43b5e1b77ed90f52d83f58
guadamartinez1306 <guadamartinez1306@gmail.com>
2025-06-25 05:02:09 -0300

    22

--- frmPrincipal.Designer.cs
+++ frmPrincipal.Designer.cs
@@ -142,6 +142,7 @@
             // 
             // statusStrip1
             // 
+            this.statusStrip1.BackColor = System.Drawing.Color.Linen;
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.lblUsuario,
